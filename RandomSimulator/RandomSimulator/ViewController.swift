f7957b2cde317585cf97b22f364b5c672ead706a
Nat <natdring97@gmail.com>
2019-03-06 16:15:47 -0800

    Added button and text functionality

---
+++ RandomSimulator/RandomSimulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomSimulator
+//
+//  Created by Nat on 2019-03-06.
+//  Copyright © 2019 Nat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
